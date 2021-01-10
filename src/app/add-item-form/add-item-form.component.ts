5033fd168221131e140ba655554dbff21e8057b4
Sanhita Sen <sanhitasen1997@gmail.com>
2021-01-10 17:25:36 +0530

    Second Commit

--- src/app/add-item-form/add-item-form.component.ts
+++ src/app/add-item-form/add-item-form.component.ts
@@ -8,7 +8,7 @@ import { BudgetItem } from 'src/shared/models/budget-item.model';
   templateUrl: './add-item-form.component.html',
   styleUrls: ['./add-item-form.component.scss']
 })
-export class AddItemFormComponent implements OnInit {
+export class AddItemFormComponent implements OnInit { 
 
   @Input() item: BudgetItem = new BudgetItem('', null);
   @Output() formSubmit: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
